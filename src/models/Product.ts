e0c7a7cff76294036c24fbdde101ef7df5220c91
Moschifire <markoladeinde@gmail.com>
2026-02-07 18:13:24 +0100

    mongoose dependencies added

---
+++ src/models/Product.ts
@@ -0,0 +1,21 @@
+import mongoose, { Schema, model, models } from "mongoose";
+
+const ProductSchema = new Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  category: { 
+    type: String, 
+    required: true, 
+    enum: ["AuraéSkin", "AuraéBeauty", "AuraéAdorn", "AuraéLiving"] 
+  },
+  images: [{ type: String }], // Array of Cloudinary URLs
+  stock: { type: Number, default: 0 },
+  details: {
+    ingredients: String, // For Skin/Beauty
+    materials: String,    // For Adorn
+  },
+}, { timestamps: true });
+
+const Product = models.Product || model("Product", ProductSchema);
+export default Product;
